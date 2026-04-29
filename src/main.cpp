4b314aab1c16fc9d0b01ec9a4e050b28fe64dab3
Marvin Gertler <marvingertler@gmail.com>
2026-04-29 10:05:59 +0200

    Menu new

--- src/main.cpp
+++ src/main.cpp
@@ -139,7 +139,7 @@ void Alarm(float u1, float u2) {
      
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Menu:");
+    lcd.print("      Menu:");
     lcd.setCursor(0, 1);
     lcd.print("S1=L S2=R S3=I");
   
